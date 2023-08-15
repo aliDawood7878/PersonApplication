58ccd1ae5e00dc33bface13fe718d8d100d9edef
aliDawood7878 <syedalidawood78@gmail.com>
2023-08-14 21:05:46 -0500

    Car Pojo implemented

--- PersonApp/src/main/java/org/example/Main.java
+++ PersonApp/src/main/java/org/example/Main.java
@@ -2,8 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Person ob = new Person("Syed",100);
+//        Person ob = new Person("Syed",100);
 
+        Car ob = new Car("Ford","2020");
+        ob.displayInfo();
 
     }
 }
